cc70c20ace14ef3b6114c52ee8ba2b53b05c83ca
JeonSeongBae <tjdqo6042@naver.com>
2018-04-11 18:07:42 +0900

    make directory

---
+++ lab01/HierarchicalClustering.R
@@ -0,0 +1,21 @@
+# Creates a sample dataset
+y <- matrix(rnorm(50), 10, 5, dimnames=list(paste("g", 1:10, sep = ""), paste("t", 1:5, sep = "")))
+
+# Compute the similarity using the spearman coefficient
+c <- cor(t(y), method="spearman")
+d <- as.dist(1-c)
+
+# Perform hierarchical clustering
+hr <- hclust(d, method = "complete", members = NULL)
+
+# plot the results
+par(mfrow = c(2,2))
+plot(hr, hang = 0.1)
+plot(hr, hang = -1)
+plot(as.dendrogram(hr), edgePar = list(col = 3, lwd = 4), horiz = T)
+
+# Prints dendrogram structure as text
+str(as.dendrogram(hr))
+
+# Prints the row labels in the order they appear in the tree
+hr$labels[hr$order]
