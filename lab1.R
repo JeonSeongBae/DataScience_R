f53621ea6f4907da7f229d82f1c5ef9ec2d70a23
JeonSeongBae <tjdqo6042@naver.com>
2018-04-11 17:54:27 +0900

    lab1_daisy()_dissimilarity matrix computation

--- lab1.R
+++ lab1.R
@@ -18,3 +18,9 @@ table(iris$Species, kc$cluster)
 
 plot(newiris[,c("Sepal.Length", "Sepal.Width")], col = kc$cluster)
 points(kc$centers[,c("Sepal.Length", "Sepal.Width")], col = 1:3, pch = 8, cex=2)
+
+# sillhouette
+library(cluster)
+dissE <- daisy(newiris)
+sk <- silhouette(kc$cl, dissE)
+plot(sk)
